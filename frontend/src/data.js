e8b9fcf774a17acc7c4e3a61a0419ecc197728c5
unknown <paul.lautier@ynov.com>
2021-01-25 12:52:16 +0100

    définition de l'objet produit dans data.js

---
+++ frontend/src/data.js
@@ -0,0 +1,81 @@
+const data = {
+    products : [
+        {
+            _id:'1',
+            name:'Menu 1',
+            image:'/images/p1.jpg',
+            price:12,
+            restaurant:'mcdonalds',
+            rating:4,
+            numReviews:100,
+            description:'triple cheese burger avec frites'
+
+        },
+        {
+            _id:'2',
+            name:'Menu 2',
+            image:'/images/p2.jpg',
+            price:10,
+            restaurant:"O'tacos",
+            rating:4.5,
+            numReviews:96,
+            description:'tacos 3 viandes'
+
+        },
+        {
+            _id:'3',
+            name:'Menu 3',
+            image:'/images/p3.jpg',
+            price:15,
+            restaurant:'dominos',
+            rating:5,
+            numReviews:56,
+            description:'pizza calzone'
+
+        },
+        {
+            _id:'4',
+            name:'Menu 4',
+            image:'/images/p4.jpg',
+            price:12,
+            restaurant:'chinois gourmand',
+            rating:4,
+            numReviews:100,
+            description:'riz blanc porc au caramel'
+
+        },
+        {
+            _id:'5',
+            name:'Menu 5',
+            image:'/images/p5.jpg',
+            price:12,
+            restaurant:'mcdonalds',
+            rating:4,
+            numReviews:100,
+            description:'triple cheese burger avec frites'
+
+        },
+        {
+            _id:'6',
+            name:'Menu 6',
+            image:'/images/p6.jpg',
+            price:12,
+            restaurant:'mcdonalds',
+            rating:4,
+            numReviews:100,
+            description:'triple cheese burger avec frites'
+
+        },
+        {
+            _id:'7',
+            name:'Menu 7',
+            image:'/images/p7.jpg',
+            price:12,
+            restaurant:'mcdonalds',
+            rating:4,
+            numReviews:100,
+            description:'triple cheese burger avec frites'
+
+        },
+    ],
+};
