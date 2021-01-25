a6bd45f67c8ad8eb602add825564159b89eb3a71
unknown <paul.lautier@ynov.com>
2021-01-25 12:54:22 +0100

    update css / passage a className

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">amazona</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+          <div className="row center">
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P1.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P1.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P1.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P1.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P1.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P1.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+
+                <div className="card">
+                    <a href="product.html">
+                        <img className="medium" src="./images/P2.jpg" alt="product"></img>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>test1</h2>
+                        </a>
+                    </div>
+
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star-half-o"></i></span>
+                        <span><i className="fa fa-star-o"></i></span>
+                    </div>
+                    <div className="price">120€</div>
+                </div>
+          </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
