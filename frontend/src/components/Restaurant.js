844c4c7f751fd858871712064c70d1d077627b83
paul-lautier <paul.lautier@ynov.com>
2021-01-25 18:04:23 +0100

    ajout d'un composant pour l'affiche des restaurant

---
+++ frontend/src/components/Restaurant.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Rating from './Rating'
+
+
+export default function Restaurant(props) {
+    const { restaurant } = props
+    return (
+<div key={restaurant._id} className="card">
+      <a href={`/restaurant/${restaurant._id}`}>
+        <img className="medium" src={restaurant.logo} alt={restaurant.name} />
+      </a>
+      <div className="card-body">
+        <a href={`/restaurant/${restaurant._id}`}>
+          <h2>{restaurant.name}</h2>
+        </a>
+       <Rating rating={restaurant.rating_r} numReviews={restaurant.numReviews_r}></Rating>
+      </div>
+    </div>
+    )
+}
