a535ed80dbbd665d1a0bb3eace4cbb5f83fe088e
paul-lautier <paul.lautier@ynov.com>
2021-01-27 09:44:39 +0100

    .gitignore fix

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import Product from '../components/Product';
-import data from '../data';
+
 
 export default function HomeScreen() {
+  const [products ,setProducts] = useState ([]);
+  useEffect(() =>{
+    const fetchData = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);}
+      fetchData();
+  }, []);
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product._id} product={product}></Product>
         ))}
       </div>
