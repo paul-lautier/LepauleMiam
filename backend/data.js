7c775728239bbe42556ca18291a29ce87448b708
paul-lautier <paul.lautier@ynov.com>
2021-01-27 09:33:56 +0100

    création server node express contenant data.js

---
+++ backend/data.js
@@ -0,0 +1,51 @@
+const data = {
+    products : [
+        {
+            _id:'1',
+            restaurantId:'1',
+            name:'Menu 1',
+            image:'/images/p1.jpg',
+            enStock:"1",
+            price:12,
+            restaurant:'Mc Donalds',
+            rating:4,
+            numReviews:100,
+            description:'triple cheese burger avec frites'
+
+        },
+        {
+            _id:'2',
+            restaurantId:'2',
+            name:'Menu 2',
+            image:'/images/p2.jpg',
+            enStock:"0",
+            price:10,
+            restaurant:"O'tacos",
+            rating:4.5,
+            numReviews:96,
+            description:'tacos 3 viandes'
+
+        },
+        
+    ],
+
+    // restaurant:[
+    //     {
+    //         _id:'1',
+    //         name:'Mc Donalds',
+    //         logo:'/logo/l1.jpg',
+    //         rating_r:4,
+    //         numReviews_r:1000,
+    //         adress:'4 rue de la paix'
+    //     },
+    //     {
+    //         _id:'2',
+    //         name:"O'Tacos",
+    //         logo:'/logo/l2.jpg',
+    //         rating_r:3,
+    //         numReviews_r:674,
+    //         adress:'6 avenue dupont'
+    //     },
+    // ],
+};
+export default data;
